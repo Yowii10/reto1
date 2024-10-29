62bbf921b75601cc7734fb70f32cbe67ebf1518b
Carlos809-stack <carloscalvobernal@gmail.com>
2024-10-29 10:09:12 +0100

    creada funciones 2

--- reto1/src/reto1/Funciones2.java
+++ reto1/src/reto1/Funciones2.java
@@ -1,5 +1,28 @@
 package reto1;
-
+import java.util.Scanner;
 public class Funciones2 {
-
+	public static  int multiplicar(int n1, int n2) {
+		return n1 * n2; 
+}
+	public static int division(int n1, int n2) {
+		if(n2==0) {
+			System.out.println("no es posible realizar la operación");
+			return -1;
+		}
+		return n1 / n2;
+	}
+	public static boolean esPositivo(int n1) {
+		if(n1>0) {
+			return true;
+		}else {
+			return false;
+		}
+}
+	public static boolean esNegativo(int n1) {
+		if(n1<0) {
+			return true;
+		}else {
+			return false;
+		}
+}
 }
