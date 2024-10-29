2b68327b8a2656bf14c7dc67c38b6aa00c69fc9d
Yowii10 <yoeliranzo@gmail.com>
2024-10-29 10:08:34 +0100

    Creada funciones 1

--- reto1/src/reto1/Funciones1.java
+++ reto1/src/reto1/Funciones1.java
@@ -2,4 +2,28 @@ package reto1;
 
 public class Funciones1 {
 
+
+	public static int suma(int n1, int n2) {
+		return n1+n2;
+	
+	}
+	
+	public static int resta(int n1, int n2) {
+		return n1-n2;
+		}
+	
+		public static boolean esPar(int num) {
+			if (num % 2 == 0) {
+				return true;
+			} else
+				return false;
+	}
+		
+		public static boolean esImpar(int num) {
+			if (num % 2 != 0) {
+				return true;
+			} else
+				return false;
+
+}
 }
